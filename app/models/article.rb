5bc068e2eced406af6ea73dc2f93c4d2e90a7299
Tiya <yaleb.t@gmail.com>
2016-08-16 18:51:32 -0700

    removed additional comment files and added comment association to article

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,5 +1,6 @@
 class Article < ApplicationRecord
     #This ensures that all articles have a title that has min 5 characters
+    has_many :comments
     validates :title, presence: true,
     length: { minimum: 5 }
 end
--- db/migrate/20160811234201_adding_body_and_article_references.rb
+++
@@ -1,6 +0,0 @@
-class AddingBodyAndArticleReferences < ActiveRecord::Migration[5.0]
-  def change
-    add_column :comments, :body, :text
-    add_column :comments, :article, :references
-  end
-end
