d52b47774ccdd37dcfa46b264d3788da756818e5
Tiya <yaleb.t@gmail.com>
2016-08-30 13:52:41 -0700

    Updated models to include new migrations

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,6 +1,5 @@
 class Article < ApplicationRecord
-    #This ensures that all articles have a title that has min 5 characters
-    has_many :comments
-    validates :title, presence: true,
+    validates :title, 
+    presence: true, 
     length: { minimum: 5 }
 end
